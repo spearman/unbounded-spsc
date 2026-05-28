1baaeb307f962a869fe9ba31177185d766d1758b
Shane Pearman <spearman@gitlab.com>
2026-05-28 23:00:10 +0200

    remove rust-analyzer from shell.nix, check for rg command in pre-push githook

--- shell.nix
+++ shell.nix
@@ -1,11 +1,10 @@
 with import <nixpkgs> {};
-pkgs.mkShell {
+mkShell {
   buildInputs = [
     cargo-udeps
     gdb # required for rust-gdb
     gh
     rustup
-    rust-analyzer
     yamllint
   ];
 }
